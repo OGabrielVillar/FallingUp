9c13db80749883e4047dad71e87fad9232337877
OGabrielVillar <gabrielvillarsqui@gmail.com>
2023-04-30 15:12:57 -0300

    First Addition

---
+++ Source/FallingUp/FallingUp.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FallingUp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FallingUp, "FallingUp" );
+ 
